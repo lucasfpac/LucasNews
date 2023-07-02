be8ca0d41b92795c1971be900e3d5a12fa629134
lucasfpac <lucasfortunato1@gmail.com>
2023-07-02 13:40:23 +0100

    API

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -6,8 +6,8 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class NewsService {
-  private apiKey = 'b4893141e40b4d2085483fde98f82558';
-  private apiUrl = 'https://newsapi.org/v2/';
+  private apiKey = '';
+  private apiUrl = '';
 
   constructor(private http: HttpClient) {}
 
